8cb7df1ad231ed4b3cc42fd67f0a8a4935b9022f
Bartek Karge <bob-ziomal@MacBook-Pro-Bartek.local>
2022-12-19 23:02:31 +0100

    first commit

--- components/Nav.js
+++ components/Nav.js
@@ -1,32 +1,41 @@
 import Link from "next/link";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../utils/firebase";
+import Image from "next/image";
+import logo from "../assets/key.png";
 
 export default function Nav() {
   const [user, loading] = useAuthState(auth);
 
   return (
-    <nav className="flex justify-between items-center py-10">
-      <Link href="/">Logo</Link>
-      <ul className="flex items-center gap-10">
-        <a className="text-lg font-medium" href="#">
-          Courses
+    <nav className="flex justify-between items-center py-10 max-w-6xl mx-auto">
+      <Link href={"/"}>
+        <a>
+          <Image
+            src={logo}
+            height={48}
+            width={48}
+            className="cursor-pointer"
+            alt="logo"
+          />
         </a>
+      </Link>
+      <ul>
         {!user && (
-          <Link href="/auth/login">
-            <a className="py-2 px-4 text-lg bg-teal-500 text-white rounded-lg font-medium ml-8">
-              Join now
+          <Link legacyBehavior href={"/auth/login"}>
+            <a className="py-2 px-4 text-lg bg-black hover:bg-gray-700 text-white rounded-lg font-medium ml-8">
+              Dołącz
             </a>
           </Link>
         )}
         {user && (
-          <div>
-            <Link href="/dashboard">
+          <div className="cursor-pointer">
+            <Link legacyBehavior href={"/dashboard"}>
               <img
-                referrerPolicy="no-referrer"
-                className="w-12 rounded-full"
                 src={user.photoURL}
-                alt=""
+                alt="avatar"
+                referrerPolicy="no-referrer"
+                className="rounded-full w-12"
               />
             </Link>
           </div>
--- utils/firebase.js
+++ utils/firebase.js
@@ -6,13 +6,20 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  // apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  // authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  // projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  // storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  // appId: process.env.NEXT_PUBLIC_APP_ID,
+  // measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyCMDh-8gemVVXgghByylwkzw-H6x-9gdj0",
+  authDomain: "react-auth-aa4a0.firebaseapp.com",
+  projectId: "react-auth-aa4a0",
+  storageBucket: "react-auth-aa4a0.appspot.com",
+  messagingSenderId: "1012996546086",
+  appId: "1:1012996546086:web:d57d685cd019ca3c8ad0e7",
+  measurementId: "G-R8MG0JZKRS",
 };
 
 // Initialize Firebase
