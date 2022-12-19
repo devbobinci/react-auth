871607f2ca5eeb76850b8bc7be7aa02e59f4d2bf
Bartek Karge <bob-ziomal@MacBook-Pro-Bartek.local>
2022-12-19 17:27:38 +0100

    first commit

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,14 +5,14 @@ import { useRouter } from "next/router";
 export default function Dashboard() {
   const route = useRouter();
   const [user, loading] = useAuthState(auth);
-  console.log(user);
   if (loading) return <h1>Loading</h1>;
   if (!user) route.push("/auth/login");
-  if (user)
+  if (user) {
     return (
       <div>
         <h1>Welcome to your dashboard {user.displayName}</h1>
         <button onClick={() => auth.signOut()}>Sign out</button>
       </div>
     );
+  }
 }
